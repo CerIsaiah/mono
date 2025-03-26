a38dd376e2c8204a661e6e5b18b414a4df533f55
Isaiah Cerven <icisaiahcerven@gmail.com>
2025-03-26 05:30:07 -0400

    porting 3

--- apps/web/smoothrizz_proj/supabase/migrations/20240101000000_add_subscription_type.sql
+++
@@ -1,6 +0,0 @@
--- Add subscription_type column to users table
-ALTER TABLE users ADD COLUMN subscription_type TEXT NOT NULL DEFAULT 'standard';
-
--- Add constraint to ensure subscription_type is either 'standard' or 'premium'
-ALTER TABLE users ADD CONSTRAINT valid_subscription_type 
-  CHECK (subscription_type IN ('standard', 'premium')); 
