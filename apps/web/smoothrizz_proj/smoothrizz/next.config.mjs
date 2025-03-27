581c6df84d13fd3ad0e8b5922d420787cca93a25
Isaiah Cerven <icisaiahcerven@gmail.com>
2025-03-27 12:57:50 -0400

    page added and lots api + db

--- apps/web/smoothrizz_proj/smoothrizz/next.config.mjs
+++ apps/web/smoothrizz_proj/smoothrizz/next.config.mjs
@@ -3,17 +3,6 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: '/',
-        has: [
-          {
-            type: 'host',
-            value: 'www.smoothrizz.com',
-          },
-        ],
-        destination: 'https://smoothrizz.com',
-        permanent: true,
-      },
-      {
         source: '/:path*',
         has: [
           {
