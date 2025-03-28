e78b105f24b4de48fdc3fdb4f517106be12be4fa
Isaiah Cerven <icisaiahcerven@gmail.com>
2025-03-28 18:56:25 -0400

    page added and lots api + db

--- apps/api/src/shared/constants.ts
+++ apps/api/src/shared/constants.ts
@@ -1,4 +1,4 @@
-export const ANONYMOUS_USAGE_LIMIT = 3;
+export const ANONYMOUS_USAGE_LIMIT = 14;
 export const FREE_USER_DAILY_LIMIT = 10;
 export const SIGNED_IN_USAGE_LIMIT = 20;
 export const FREE_MAX_PERCENTAGE = 0.7;
