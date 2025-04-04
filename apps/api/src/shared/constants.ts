573cfae483cab22c85d6e54391bf475cc28d06ed
Isaiah Cerven <icisaiahcerven@gmail.com>
2025-04-04 00:13:53 -0400

    page added and lots api + db

--- apps/api/src/shared/constants.ts
+++ apps/api/src/shared/constants.ts
@@ -5,6 +5,6 @@ export const FREE_USER_DAILY_LIMIT = 14;
 // Learning percentage constants
 export const MIN_LEARNING_PERCENTAGE = 0;
 export const FREE_INCREMENT_PER_RESPONSE = 5;
-export const PREMIUM_INCREMENT_PER_RESPONSE = 10;
+export const PREMIUM_INCREMENT_PER_RESPONSE = 7;
 export const FREE_MAX_PERCENTAGE = 70;
 export const PREMIUM_MAX_PERCENTAGE = 100; 
